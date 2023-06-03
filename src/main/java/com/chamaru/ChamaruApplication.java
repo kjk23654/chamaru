a1da0582645978457c37e4340f6cb7d8acd86f9b
jungmin0917 <cloonds@gmail.com>
2023-06-03 12:21:57 +0900

    차마루 - 첫 커밋

---
+++ src/main/java/com/chamaru/ChamaruApplication.java
@@ -0,0 +1,13 @@
+package com.chamaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamaruApplication.class, args);
+    }
+
+}
